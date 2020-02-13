175b5437e6883d400bd3ca22c3ec6eea8f44c89c
JonathanCanche <jhonathancanchem@gmail.com>
2020-02-13 13:39:50 -0600

    ultimos cambios

--- src/app/components/landing/landing.component.ts
+++ src/app/components/landing/landing.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import{WebData} from 'src/app/interface'
 
 import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
@@ -17,6 +17,13 @@ export class LandingComponent implements OnInit {
   
   private itemDoc: AngularFirestoreDocument<WebData>;
   item: Observable<any>;
+
+  @ViewChild('buttonmenu') buttonmenu: ElementRef;
+  @ViewChild('home_li') homeLi: ElementRef;
+  @ViewChild('servicios_li') serviciosLi: ElementRef;
+  @ViewChild('about_li') aboutLi: ElementRef;
+  @ViewChild('contact_li') contactLi: ElementRef;
+
   constructor(private afs: AngularFirestore,private router: Router, private routes: ActivatedRoute) { 
     this.itemDoc=afs.doc<WebData>('webs/pruebasalud');
     this.item=this.itemDoc.valueChanges();
@@ -33,7 +40,7 @@ export class LandingComponent implements OnInit {
       this.itemDoc = this.afs.doc<any>(`webs/${id}`);
       this.item = this.itemDoc.valueChanges();
       this.item.subscribe((data) => {
-        console.log('data', data);
+        // console.log('data', data);
         if (data === undefined || data.tipo != 5) {
           this.router.navigateByUrl('/404');
         } else {
@@ -44,7 +51,46 @@ export class LandingComponent implements OnInit {
     });
   }
 
-  scrollToElement($element): void {
+  scrollToElement($element, activeElement: string, preventClick?: boolean): void {
+    if (preventClick) {
+      this.buttonmenu.nativeElement.click();
+    }
     $element.scrollIntoView({behavior: 'smooth', block: 'start'});
+    this.activesToogle(activeElement);
+  }
+
+  activesToogle( el: string) {
+    switch (el) {
+      case 'home':
+        this.homeLi.nativeElement.classList.add('active');
+        this.serviciosLi.nativeElement.classList.remove('active');
+        this.aboutLi.nativeElement.classList.remove('active');
+        this.contactLi.nativeElement.classList.remove('active');
+        break;
+      case 'servicios':
+        this.homeLi.nativeElement.classList.remove('active');
+        this.serviciosLi.nativeElement.classList.add('active');
+        this.aboutLi.nativeElement.classList.remove('active');
+        this.contactLi.nativeElement.classList.remove('active');
+        break;
+      case 'about':
+        this.homeLi.nativeElement.classList.remove('active');
+        this.serviciosLi.nativeElement.classList.remove('active');
+        this.aboutLi.nativeElement.classList.add('active');
+        this.contactLi.nativeElement.classList.remove('active');
+        break;
+      case 'contact':
+        this.homeLi.nativeElement.classList.remove('active');
+        this.serviciosLi.nativeElement.classList.remove('active');
+        this.aboutLi.nativeElement.classList.remove('active');
+        this.contactLi.nativeElement.classList.add('active');
+        break;
+      default:
+        this.homeLi.nativeElement.classList.add('active');
+        this.serviciosLi.nativeElement.classList.remove('active');
+        this.aboutLi.nativeElement.classList.remove('active');
+        this.contactLi.nativeElement.classList.remove('active');
+        break;
+    }
   }
 }
